a5040ea183db740a4826446b43cae63e70d62ec8
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-04-22 00:40:22 +0100

    chore: setup registration fields

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::get('/', function () {
     return response()->json([
         'message' => 'Welcome to Design House API'
     ], 200);
+});
+
+// Route group for authenticated users
+Route::group(['middleware' => ['auth:api']], function () {
+});
+
+// Route group for guest users
+Route::group(['middleware' => ['guest:api']], function () {
+    Route::post('/register',  'Auth\RegisterController@register');
 });
