21cff013bcf923cbb7323c6cdf82e88583b2dd17
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-04-24 21:52:15 +0100

    feature: add user resources

--- app/Http/Controllers/User/MeController.php
+++ app/Http/Controllers/User/MeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
@@ -15,9 +16,7 @@ class MeController extends Controller
     public function getMe()
     {
         if (auth()->check()) {
-            return response()->json([
-                'user' => auth()->user()
-            ], 200);
+            return new UserResource(auth()->user());
         }
         return response()->json(null, 401);
     }
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'username' => $this->username,
+            'email' => $this->email,
+            'formatted_address' => $this->formatted_address,
+            'tagline' => $this->tagline,
+            'about' => $this->about,
+            'location' => $this->location,
+            'available_to_hire' => $this->available_to_hire,
+            'about' => $this->about,
+            'created_dates' => [
+                'created_at_human' => $this->created_at->diffForHumans(),
+                'created_at' => $this->created_at
+            ],
+            'updated_dates' => [
+                'updated_at_human' => $this->updated_at->diffForHumans(),
+                'updated_at' => $this->updated_at
+            ],
+        ];
+    }
+}
