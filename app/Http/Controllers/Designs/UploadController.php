7915790ca5e6fa6f2c8c798b421dcc495ed4fc9f
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-04-26 14:59:09 +0100

    feature: upload image to temporary disk

--- app/Http/Controllers/Designs/UploadController.php
+++ app/Http/Controllers/Designs/UploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Designs;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\UploadImage;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
@@ -11,7 +12,31 @@ class UploadController extends Controller
     {
         // validate the request
         $this->validate($request, [
-            'image' => ['required', 'mime:jpeg,gif,bmp,png', 'max:2048']
+            'image' => ['required', 'mimes:jpeg,gif,bmp,png', 'max:2048']
         ]);
+
+        // get image
+        $image = $request->file('image');
+        $image_path = $image->getPathname();
+
+        // get original filename, replace any spaces with underscore '_' and
+        // convert to lowercase and add timestamp.
+        // E.g. Business Cards.png ==> timestamp()_business_cards.png
+        $filename = time(). "_" .preg_replace('/\$+/', '_', strtolower($image->getClientOriginalName()));
+
+        // move image to temporary location (tmp)
+        $tmp = $image->storeAs('uploads/original', $filename, 'tmp');
+
+        // create the database record for the uploaded design
+        $design = auth()->user()->designs()->create([
+            'image' => $filename,
+            'disk' => config('site.upload_disk'),
+        ]);
+
+        // dispatch a job to handle image manipulation
+        $this->dispatch(new UploadImage($design));
+
+        return response()->json($design, 200);
+
     }
 }
