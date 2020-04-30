600cf91077593df2841da3896c4f53d94d99e6b9
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-05-01 00:16:26 +0100

    chore: add laravel debuggar to profile sql queries

---
+++ app/Http/Middleware/ProfileJsonResponse.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Arr;
+use Illuminate\Http\JsonResponse;
+
+class ProfileJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $response =  $next($request);
+
+        // check if debugbar is enabled
+        if (!app()->bound('debugbar') || !app('debugbar')->isEnabled()) {
+            return $response;
+        }
+
+        // profile the json response
+        if ($response instanceof JsonResponse && $request->has('_debug')) {
+            // Get the queries information from the debugbar
+            $response->setData(array_merge($response->getData(true), [
+                '_debugbar' => Arr::only( app('debugbar')->getData(), 'queries')
+            ]));
+            // $response->setData(array_merge($response->getData(true), [
+            //     '_debugbar' => app('debugbar')->getData(),
+            // ]));
+        }
+
+        return $response;
+    }
+}
